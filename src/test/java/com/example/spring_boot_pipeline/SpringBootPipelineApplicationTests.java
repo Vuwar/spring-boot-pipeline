ebab7e25419c85b5b340c3f6322c75840a58b201
Vuwar <vuqarkrm1@gmail.com>
2025-04-26 10:31:53 +0400

    Initial commit

---
+++ src/test/java/com/example/spring_boot_pipeline/SpringBootPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
